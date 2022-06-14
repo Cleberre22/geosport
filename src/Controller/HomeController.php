14946c5220bdc202dd9699e0204fae88e486c21e
Charley Le Berre <c.leberre@hotmail.fr>
2022-06-14 15:07:00 +0200

    Leaflet Js intégration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ClubRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(clubRepository $clubRepository,): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'club' => $clubRepository->findAll(),
         ]);
     }
 }
