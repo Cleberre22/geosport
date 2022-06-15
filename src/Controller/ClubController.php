f34054daf78fd8f94449d557c9e9700409f328ff
Charley Le Berre <c.leberre@hotmail.fr>
2022-06-15 11:29:27 +0200

    Page index + show + link marker (club)

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Club;
+use App\Repository\ClubRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ClubController extends AbstractController
 {
     #[Route('/club', name: 'club')]
-    public function index(): Response
+    public function index(ClubRepository $clubCrudRepository): Response
     {
         return $this->render('club/index.html.twig', [
             'controller_name' => 'ClubController',
+            'club' => $clubCrudRepository->findBy([],['name'=>'ASC']),
+        ]);
+    }
+
+    
+
+
+    #[Route('/{id}', name: 'club_show', methods: ['GET'])]
+    public function show(Club $club): Response
+    {
+        return $this->render('club/show.html.twig', [
+            'club' => $club,
         ]);
     }
 }
