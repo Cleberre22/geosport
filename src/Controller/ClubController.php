48d9417dfc0048cee8415811aaf544d0e29f1cc2
Charley Le Berre <c.leberre@hotmail.fr>
2022-06-15 15:51:25 +0200

    MAJ page show et index Club

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Club;
 use App\Repository\ClubRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,14 @@ class ClubController extends AbstractController
         return $this->render('club/index.html.twig', [
             'controller_name' => 'ClubController',
             'club' => $clubCrudRepository->findBy([],['name'=>'ASC']),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'club_show', methods: ['GET'])]
+    public function show(Club $club): Response
+    {
+        return $this->render('club/show.html.twig', [
+            'club' => $club,
         ]);
     }
 }
