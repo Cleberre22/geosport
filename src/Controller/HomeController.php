36865d3b22310da07a98f69f89af16c8eb91fe0d
Cleberre22 <c.leberre@hotmail.fr>
2022-06-20 17:01:13 +0200

    Test filtrage AJax

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ClubRepository;
+use App\Repository\SportRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(clubRepository $clubRepository,): Response
+    public function index(clubRepository $clubRepository, SportRepository $sportRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'club' => $clubRepository->findAll(),
+            'sport' => $sportRepository->findAll(),
         ]);
     }
 }
