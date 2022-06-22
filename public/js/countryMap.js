b1ddeaa2ef37c18429e9e9376ca44317c2483c5a
Cleberre22 <c.leberre@hotmail.fr>
2022-06-22 16:55:21 +0200

    Map Js

--- public/js/countryMap.js
+++ public/js/countryMap.js
@@ -1,5 +1,12 @@
-const input = document.querySelector('countryMap');
-
-input.addEventListener('click', event => {
-  countryMap.innerHTML = `Nombre de clics : ${event.detail}`;
+const countryMap = document.getElementById('countryMap');    // On récupère l'élément sur lequel on veut détecter le clic
+countryMap.addEventListener('click', function() {          // On écoute l'événement click
+    console.log('gg');               // On change le contenu de notre élément pour afficher "C'est cliqué !"
 });
+
+
+// const radios = document.getElementById('countryMap')
+// for (const radio of radios) {
+//   radio.onclick = (e) => {
+//     console.log(e.target.value);
+//   }
+// }
